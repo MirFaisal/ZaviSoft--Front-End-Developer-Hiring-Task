fa4dfc974d226d27184ec54976c5befc7f79be60
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2026-02-21 15:57:40 +0600

    feat: replace ProductCard with ProductCardBranded in ProductGrid component

--- src/components/products/ProductGrid.jsx
+++ src/components/products/ProductGrid.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import ProductCard from "./ProductCard";
+import { ProductCardBranded } from "./";
 import ProductCardSkeleton from "./ProductCardSkeleton";
 
 export default function ProductGrid({ products, isLoading, error }) {
@@ -35,7 +35,7 @@ export default function ProductGrid({ products, isLoading, error }) {
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <ProductCardBranded key={product.id} product={product} badge="" />
       ))}
     </div>
   );
